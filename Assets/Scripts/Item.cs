740a25971c86e723a51afb39970ad1a5d6794493
Ivasi228 <135738104+Ivasi228@users.noreply.github.com>
2023-06-07 00:52:10 +0700

    Initial commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class Item
+    {
+        [SerializeField] Tile _tile;
+        public Tile Tile
+        {
+            get { return _tile; }
+            set { _tile = value; }
+        }
+        [SerializeField] Tilemap _tilemap;
+        public Tilemap Tilemap
+        {
+            get { return _tilemap; }
+            set { _tilemap = value; }
+        }
+        [Range(1, 100)]
+        [SerializeField] int _chance;
+        public int Chance
+        {
+            get { return _chance; }
+            set { _chance = value; }
+        }
+        [SerializeField] bool _spawnOnGround;
+        public bool SpawnOnGround
+        {
+            get { return _spawnOnGround; }
+            set { _spawnOnGround = value; }
+        }
+        [SerializeField] bool _isGameobject;
+        public bool IsGameobject
+        {
+            get { return _isGameobject; }
+            set { _isGameobject = value; }
+        }
+        [SerializeField] GameObject _gameObject;
+        public GameObject GameObject
+        {
+            get { return _gameObject; }
+            set { _gameObject = value; }
+        }
+    }
+}
---
+++ Assets/Scripts/Patrul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Patrul : MonoBehaviour
+    {
+        Vector3 _dir = Vector3.left;
+        [SerializeField] float _speed;
+        [SerializeField] Rigidbody2D _rb;
+        private void FixedUpdate()
+        {
+                _rb.AddForce(_dir * _speed);
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            _dir *= -1;
+        }
+    }
+}
